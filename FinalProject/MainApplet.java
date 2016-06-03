94af866ed994ae93b4f5c1a1845a9fee5c82c52e
yujhen <qwa33096@gmail.com>
2016-06-03 12:36:52 +0800

    Add back button (Now can jump between page) fix some error //  button remove->hide

--- FinalProject/MainApplet.java
+++ FinalProject/MainApplet.java
@@ -13,7 +13,7 @@ import processing.data.JSONObject;
 public class MainApplet extends PApplet{
 	private final static int width = 1000, height = 700;
 	private ControlP5 cp5;
-	Button buttonA,buttonB,buttonC,buttonD,btOne,btTwo,btCharacter,btEnvironment,btAbout;
+	Button buttonA,buttonB,buttonC,buttonD,btOne,btTwo,btCharacter,btEnvironment,btAbout,btBackToMenu;
 	JSONObject data;
 	JSONArray problems;
 	JSONObject problem;
@@ -24,7 +24,7 @@ public class MainApplet extends PApplet{
 	int score;
 	private int flag=0;
 	PFont font = createFont("標楷體",20);
-	PImage character,character2,menu;
+	PImage character,character2,menu,characterPage,environmentPage,aboutPage;
 	int locationx=300,locationy=50;
 	ArrayList<PImage> monster;
 	private int i=0,j=0;
@@ -35,14 +35,19 @@ public class MainApplet extends PApplet{
 		monster=new ArrayList<PImage>();
 		character = loadImage("src/resources/img/character_1.png");
 		menu = loadImage("src/resources/main menu.png");
+		characterPage = loadImage("src/resources/shop.png");
+		environmentPage = loadImage("src/resources/setting.png");
+		aboutPage = loadImage("src/resources/setting.png");
+		
 		for(j=1;j<=5;j++)
 		{
-		character2=loadImage("src/resources/img/monster_"+j+".png");
-		monster.add(character2);
+			character2=loadImage("src/resources/img/monster_"+j+".png");
+			monster.add(character2);
 		}
 		Ani.init(this);
 		size(width,height);
 		smooth();
+		
 		cp5 = new ControlP5(this);
 
 		score = 0;
@@ -54,6 +59,15 @@ public class MainApplet extends PApplet{
 		btCharacter = cp5.addButton("btCharacter").setLabel("角色商店").setPosition(width/2, 480) .setSize(150, 50); 
 		btEnvironment = cp5.addButton("btEnvironment").setLabel("環境設定").setPosition(width/2, 540) .setSize(150, 50); 
 		btAbout = cp5.addButton("btAbout").setLabel("關於遊戲").setPosition(width/2, 600) .setSize(150, 50); 
+		
+		btBackToMenu = cp5.addButton("btBackToMenu").setLabel("Back").setPosition(800, 600) .setSize(150, 50);
+		cp5.getController("btBackToMenu")
+	     .getCaptionLabel()
+	     .setFont(f)
+	     .toUpperCase(false)
+	     .setSize(24)
+	     ;
+		btBackToMenu.hide();
 		
 		cp5.getController("btOne")
 	     .getCaptionLabel()
@@ -88,13 +102,22 @@ public class MainApplet extends PApplet{
 	     ;
 			
 	}
-	
-	public void chButton(){
-		btOne.remove();
-		btTwo.remove();
-		btCharacter.remove();
-		btEnvironment.remove();
-		btAbout.remove();
+	public void addBackButton(){
+		btOne.hide();
+		btTwo.hide();
+		btCharacter.hide();
+		btEnvironment.hide();
+		btAbout.hide();
+		btBackToMenu.show();
+
+	}
+	public void chButton(){ //get in 1-P
+		btOne.hide();
+		btTwo.hide();
+		btCharacter.hide();
+		btEnvironment.hide();
+		btAbout.hide();
+		
 		buttonA = cp5.addButton("buttonA").setLabel(problem.getString("choiceA")).setPosition(350, 420) .setSize(150, 50); 
 		buttonB = cp5.addButton("buttonB").setLabel(problem.getString("choiceB")).setPosition(350, 480) .setSize(150, 50); 
 		buttonC = cp5.addButton("buttonC").setLabel(problem.getString("choiceC")).setPosition(350, 540) .setSize(150, 50); 
@@ -129,8 +152,16 @@ public class MainApplet extends PApplet{
 	     ;
 	
 	}
-	
-	public void btOne(){
+	public void btBackToMenu(){
+		flag=0;
+		btOne.show();
+		btTwo.show();
+		btCharacter.show();
+		btEnvironment.show();
+		btAbout.show();
+		btBackToMenu.hide();
+	}
+	public void btOne(){ //1-P
 		flag=1;
 		chButton();
 		btUpdate();
@@ -139,7 +170,34 @@ public class MainApplet extends PApplet{
 		//draw();
 	}
 	
-	public void buttonA(){
+	public void btTwo(){ //2-P
+		flag=2;
+		
+		//draw();
+	}
+	public void btCharacter(){ //Character
+		flag=3;
+		addBackButton();
+		cp5.update();
+		
+		//draw();
+	}
+	public void btEnvironment(){ //Environment
+		flag=4;
+		addBackButton();
+		cp5.update();
+		
+		//draw();
+	}
+	public void btAbout(){ //About
+		flag=5;
+		addBackButton();
+		cp5.update();
+		
+		//draw();
+	}
+	
+	public void buttonA(){ //choose A
 		checkAnswer("A");
 		i++;
 		problem = problems.getJSONObject(i);
@@ -149,7 +207,7 @@ public class MainApplet extends PApplet{
 		
 		//draw();
 	}
-	public void buttonB(){
+	public void buttonB(){ //choose B
 		checkAnswer("B");
 		i++;
 		problem = problems.getJSONObject(i);
@@ -157,7 +215,7 @@ public class MainApplet extends PApplet{
 		cp5.update();
 		
 	}
-	public void buttonC(){
+	public void buttonC(){ //choose C
 		checkAnswer("C");
 		i++;
 		problem = problems.getJSONObject(i);
@@ -165,7 +223,7 @@ public class MainApplet extends PApplet{
 		cp5.update();
 		
 	}
-	public void buttonD(){
+	public void buttonD(){ //choose D
 		checkAnswer("D");
 		i++;
 		problem = problems.getJSONObject(i);
@@ -175,21 +233,31 @@ public class MainApplet extends PApplet{
 	}
 	public void draw(){
 		background(255);
-		if(flag==0){
+		if(flag==0){ //menu mode
 			image(menu,0,0,width,height);
 		}
 		
-		else if (flag==1){
-		textFont(font,20);
-		fill(0);
-		image(monster.get(0),locationx,locationy,300,300);
-		
-		line(0, height/2, width, height/2);
-		msg = problem.getString("question");
-		text(msg,20, 400);
-		text(score,width-50,height/2+20);
+		else if (flag==1){ //1-P mode
+			textFont(font,20);
+			fill(0);
+			image(monster.get(0),locationx,locationy,300,300);
+			
+			line(0, height/2, width, height/2);
+			msg = problem.getString("question");
+			text(msg,20, 400);
+			text(score,width-50,height/2+20);
 		}
-	
+		
+		else if(flag==2){ //2-P mode
+			
+		}else if(flag==3){ //Character mode
+			image(characterPage,0,0,width,height);
+		}else if(flag==4){ //Environment mode
+			image(environmentPage,0,0,width,height);
+		}else if(flag==5){ //About mode
+			image(aboutPage,0,0,width,height);
+
+		}
 	}
 	public void checkAnswer(String ans){
 		answer = problem.getString("answer");
@@ -212,11 +280,12 @@ public class MainApplet extends PApplet{
 			System.out.println("答錯");
 		
 	}
-	private void btUpdate(){
-		buttonA.setLabel(problem.getString("choiceA"));
-		buttonB.setLabel(problem.getString("choiceB"));
-		buttonC.setLabel(problem.getString("choiceC"));
-		buttonD.setLabel(problem.getString("choiceD"));
+	private void btUpdate(){ //date button index
+			buttonA.setLabel(problem.getString("choiceA"));
+			buttonB.setLabel(problem.getString("choiceB"));
+			buttonC.setLabel(problem.getString("choiceC"));
+			buttonD.setLabel(problem.getString("choiceD"));
+		
 	}
 	private void loadData(){
 		data = loadJSONObject(file);
